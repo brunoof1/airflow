52ebc80b11d8f60a9860c2496a03b0e46c1c9b0d
brunoof1 <brunoof1@hotmail.com>
2019-11-30 10:00:29 -0300

    Update google_sheets_hook.py
    Ajusting identation and remoing unused modules

--- google_sheets_hook.py
+++ google_sheets_hook.py
@@ -1,5 +1,4 @@
 import os
-from pprint import pprint
 from googleapiclient import discovery
 
 from airflow.hooks.base_hook import BaseHook
@@ -11,7 +10,6 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/home/airflow/airflow/credential
 class GoogleSheetsHook(BaseHook):
     def __init__(self, google_conn_id='google_default'):
         self.connection = self.get_connection(google_conn_id)
-        print('DEBUG 1' ,self.connection)
 
     def get_service_object(self,
                            api_name,
@@ -20,7 +18,6 @@ class GoogleSheetsHook(BaseHook):
 
         if os.getenv("GOOGLE_APPLICATION_CREDENTIALS") is not None:
             credentials = None
-            print('DEBUG 3', api_name, api_version, credentials)
 
         return discovery.build(api_name, api_version, credentials=credentials, cache_discovery=False)
 
