4b5d360121e4ddb1d9e0305dbd5fcfa16404690e
brunoof1 <brunoof1@hotmail.com>
2019-12-01 14:20:36 -0300

    Update S3_File_Sensor_dag.py
    Removing unused components

--- S3_File_Sensor_dag.py
+++ S3_File_Sensor_dag.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from airflow.operators import SimpleHttpOperator, HttpSensor, BashOperator, EmailOperator, S3KeySensor
+from airflow.operators import S3KeySensor
 from datetime import datetime, timedelta
 
 default_args = {
@@ -17,8 +17,6 @@ with DAG('S3_File_Sensor_dag.py',
         default_args=default_args,
         schedule_interval= '@once') as dag:
 
-        t1 = BashOperator(task_id='bash_task', bash_command='echo "hello world" > s3_conn_test.txt', dag=dag)
+        sensor = S3KeySensor(task_id='S3Key_task', bucket_key='AirflowLogs/teste.txt', wildcard_match=True, bucket_name='bruno-souza-datalake', s3_conn_id='s3_default', timeout=18*60*60, poke_interval=120, dag=dag)
 
-        sensor = S3KeySensor(task_id='S3Key_task', bucket_key='AirflowLogs/teste.txt', wildcard_match=True, bucket_name='levee-datalake', s3_conn_id='s3_default', timeout=18*60*60, poke_interval=120, dag=dag)
-
-        t1.set_upstream(sensor)
+        sensor
